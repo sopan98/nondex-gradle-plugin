7a6a16237d499ab5ada74b60c6703662feb74892
sopan98 <phaltankarsopan@gmail.com>
2022-10-16 09:42:51 -0500

    Try to run tests

--- plugin/src/main/java/edu/illinois/nondex/gradle/plugin/tasks/NonDexTest.java
+++ plugin/src/main/java/edu/illinois/nondex/gradle/plugin/tasks/NonDexTest.java
@@ -2,31 +2,33 @@ package edu.illinois.nondex.gradle.plugin.tasks;
 
 import edu.illinois.nondex.common.ConfigurationDefaults;
 import edu.illinois.nondex.common.Utils;
-import edu.illinois.nondex.instr.Main;
+import org.gradle.api.logging.LogLevel;
 import org.gradle.api.tasks.testing.Test;
-import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.Project;
+
+import org.gradle.api.logging.Logger;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class NonDexTest extends Test {
     Project project = getProject();
+    Logger logger = project.getLogger();
 
     public static final String TASK_NAME = "nondexTest";
 
     public void init() {
         setDescription("Test with NonDex");
         setGroup("NonDex");
-    }
-
-    @TaskAction
-    public void nondexTest() throws Exception {
-        String commonPath = project.getBuildscript().getConfigurations().getByName("classpath").getAsPath();
-        String outPath = project.getBuildDir().getAbsolutePath() + File.separator + "out.jar";
-        Main.main(outPath);
-        String args = "-Xbootclasspath/p:" + outPath + File.pathSeparator + commonPath;
-        setJvmArgs(Arrays.asList(new String[]{args, "-D" + ConfigurationDefaults.PROPERTY_EXECUTION_ID + "=" + Utils.getFreshExecutionId()}));
-        System.out.println("Running with arguments: " + getJvmArgs());
+        doFirst(task -> {
+            String classpath[] = System.getProperty("java.class.path").split(";");
+            String instrumentedJarPath = Arrays.stream(classpath).filter(str -> str.contains("nondex-instrumentation")).collect(Collectors.joining());
+            String commonJarPath = Arrays.stream(classpath).filter(str -> str.contains("nondex-common")).collect(Collectors.joining());
+            String args = "-Xbootclasspath/p:" + instrumentedJarPath + File.pathSeparator + commonJarPath;
+            String jvmArgs[] = new String[]{args, "-D" + ConfigurationDefaults.PROPERTY_EXECUTION_ID + "=" + Utils.getFreshExecutionId()};
+            setJvmArgs(Arrays.asList(jvmArgs));
+            logger.log(LogLevel.DEBUG,"Running with arguments: " + getJvmArgs());
+        });
     }
 }
